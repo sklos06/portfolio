592efd4842b26e9c4eab173feef7fefa6a5e2266
Szymon <sklos0620@gmail.com>
2023-07-05 12:43:21 +0200

    menu

---
+++ src/Menu.tsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Menu(){
+
+    return(
+        <div className="menu">
+            <button className="menu-button">Social media</button>
+            <button className="menu-button">Contact</button>
+        </div>
+    )
+}

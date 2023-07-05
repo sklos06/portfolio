f420fdbe850f464ee131f762509c707438349be0
Szymon <sklos0620@gmail.com>
2023-07-05 13:07:37 +0200

    content

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Menu from "./Menu"
+import Content from "./Content"
 function App() {
   return (
     <div className="App">
       <Menu/>
+        <Content/>
     </div>
   );
 }
---
+++ src/Content.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function Content(){
+    return(
+        <main>
+            <div className="content-leftSide">
+                <h1 className="name">Szymon Kłos</h1>
+                <h3 className="developer">Front-end developer</h3>
+                <p className="description">
+                    I am an ambitious and dedicated individual
+                    seeking new challenges in my career. I aspire
+                    to develop my skills and gain valuable
+                    experience. My main goals involve leveraging
+                    my skills and knowledge to achieve success
+                    in an inspiring and growth-oriented
+                    environment.
+                </p>
+            </div>
+            <div className="content-rightSide">
+                <img className="photo" alt="my photo"src={"./photo.jpg"}/>
+            </div>
+        </main>
+    )
+}

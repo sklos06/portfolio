f5573218785380a2c82b8d61481c77b7fbd1be49
Szymon Kłos <52749568+sklos06@users.noreply.github.com>
2024-06-19 22:44:17 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         <Menu changeMedia={handleChangeMedia}  changeMail={handleChangeMail} getMail={projects}/>
         {socialMedia &&
             <div className="social">
-                <a className="social-reference" href="https://github.com/sklos06"><img src="./github.png"/><span>Github</span></a>
+                <a className="social-reference" href="https://github.com/sklos06"><img src="./github.png" alt="github"/><span>Github</span></a>
                 <a className="social-reference" href="https://www.linkedin.com/in/szymon-kłos-358518278/"><img src="./linkedln.png"/><span>Linkedln</span></a>
                 <a className="social-reference" href="https://www.instagram.com/sklos0620/"><img src="./instagram.png"/><span>Instagram</span></a>
                 <a className="social-reference" href="https://www.facebook.com/profile.php?id=100006437774924"><img src="./facebook.png"/><span>Instagram</span></a>
